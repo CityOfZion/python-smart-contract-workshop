4e702379327fd41070f57d4e9926ca580beda65e
Chris Hager <chris@linuxuser.at>
2018-02-01 12:41:16 -0800

    1-print example: multiple args as payload

--- 1-print.py
+++ 1-print.py
@@ -6,6 +6,10 @@ Invoke:       neo> testinvoke <hash> main
 from boa.blockchain.vm.Neo.Runtime import Log, Notify
 
 def Main():
-    print("log via print 1")
-    Log("log 2")
-    Notify("notify2")
+    print("log via print (1)")
+    Log("normal log (2)")
+    Notify("notify (3)")
+
+    # Sending multiple arguments as notify payload:
+    msg = ["a", 1, 2, b"3"]
+    Notify(msg)
