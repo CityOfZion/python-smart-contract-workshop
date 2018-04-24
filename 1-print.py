9807e5e94ca3980d4285c08be070d0e6ed91988a
Chris Hager <chris@linuxuser.at>
2018-04-24 13:23:57 +0200

    Update 1-print.py

--- 1-print.py
+++ 1-print.py
@@ -2,7 +2,7 @@
 In prompt.py, you need to execute `config sc-events on` to see the events showing up.
 
 Test & Build:
-neo> build sc/1-print.py test 07 05 True False
+neo> build 1-print.py test 07 05 True False
 """
 
 def Main():
