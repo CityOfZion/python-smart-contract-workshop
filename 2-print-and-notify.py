1dee41e89ce6d4a45de67ec961a6ab066f31cf2b
Chris Hager <chris@linuxuser.at>
2018-04-24 13:24:12 +0200

    Update 2-print-and-notify.py

--- 2-print-and-notify.py
+++ 2-print-and-notify.py
@@ -2,7 +2,7 @@
 In prompt.py, you need to execute `config sc-events on` to see the events showing up.
 
 Test & Build:
-neo> build sc/2-print-and-notify.py test 07 05 True False
+neo> build 2-print-and-notify.py test 07 05 True False
 """
 from boa.interop.Neo.Runtime import Log, Notify
 
